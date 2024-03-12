f90e95e48f2355bd79bb13dc4fd81aaf58c125d1
AnonymDavid <jodavid512@gmail.com>
2024-03-12 09:02:19 +0100

    Adding quaternion function overloads

--- python/helper_functions/quaternion_tools.py
+++ python/helper_functions/quaternion_tools.py
@@ -17,6 +17,22 @@ def euler_to_quaternion(roll:float, pitch:float, yaw:float, in_degrees:bool=True
     """
     return R.from_euler('xyz', [roll, pitch, yaw], degrees=in_degrees).as_quat()
 
+def euler_to_quaternion(rpy:ndarray, in_degrees:bool=True) -> ndarray:
+    """
+    Convert Euler angles to quaternion.
+
+    Args:
+        rpy (numpy.ndarray): The roll, pitch, and yaw angles in degrees.
+        in_degrees (bool, optional): If the input angles are in degrees. Defaults to True.
+
+    Returns:
+        numpy.ndarray: The quaternion representation of the Euler angles [x,y,z,w].
+    """
+    if rpy.shape != (3,):
+        raise ValueError("The input euler angles must be a 3x1 array.")
+
+    return R.from_euler('xyz', rpy, degrees=in_degrees).as_quat()
+
 def quaternion_to_euler(x:float, y:float, z:float, w:float, in_degrees:bool=True) -> ndarray:
     """
     Convert an quaternion to euler angles.
@@ -32,3 +48,19 @@ def quaternion_to_euler(x:float, y:float, z:float, w:float, in_degrees:bool=True
         numpy.ndarray: The euler representation of the quaternion [roll, pitch, yaw].
     """
     return R.from_quat([x, y, z, w]).as_euler('xyz', degrees=in_degrees)
+
+def quaternion_to_euler(quat:ndarray, in_degrees:bool=True) -> ndarray:
+    """
+    Convert an quaternion to euler angles.
+
+    Args:
+        quat (numpy.ndarray): The quaternion [x, y, z, w].
+        in_degrees (bool, optional): If the output angles should be in degrees. Defaults to True.
+
+    Returns:
+        numpy.ndarray: The euler representation of the quaternion [roll, pitch, yaw].
+    """
+    if quat.shape != (4,):
+        raise ValueError("The input quaternion must be a 4x1 array.")
+    
+    return R.from_quat(quat).as_euler('xyz', degrees=in_degrees)
